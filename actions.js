8187995d8406bce37c2784d4cbcc2bc71f481246
perkins.christopher@siemens.com <perkins.christopher@siemens.com>
2017-05-26 16:00:14 -0400

    various changes to formatting

--- actions.js
+++ actions.js
@@ -1,4 +1,42 @@
-window.onload = function () {
+window.onload = function ()
+{
         document.getElementById("terminal").innerHTML += "<p>C:/chrisperkins/me " +//non-editable location
-            "<span id=\"edit\" contenteditable=\"true\"></span></p>";//editable text
+            "<span id=\"input\" contenteditable=\"true\"></span></p>";//editable text
+        document.getElementById("terminal").addEventListener("paste", handlePaste);
+        document.getElementById("terminal").addEventListener("click", focusInput());
+
+        focusInput();
+}
+
+// Disallow html paste
+function handlePaste (e)
+{
+    // Stop data being directly pasted into div
+    e.stopPropagation();
+    e.preventDefault();
+
+    /* Get pasted data via clipboard API
+    clipboardData = e.clipboardData || window.clipboardData;
+    pastedData = clipboardData.getData('Text');
+
+    insertAtCaret(pastedData)*/
+}
+
+function focusInput()
+{
+    document.getElementById("input").focus();
+}
+
+function brIfEnter(e)
+{
+    alert("...");
+    if (e.keyCode == 13)
+    {
+        alert("???");
+        document.getElementById("input").className = "pastInput";
+        document.getElementById("terminal").innerHTML = "<br>";
+        
+        document.getElementById("terminal").innerHTML += "<p>C:/chrisperkins/me " +//non-editable location
+                "<span id=\"input\" contenteditable=\"true\"></span></p>";//editable text
+    } 
 }
