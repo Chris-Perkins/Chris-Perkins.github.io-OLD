a0244df5ec07c8566229ccadd687b6423d3d59b4
neptunicjenkins <chrisfromtemporaryid@gmail.com>
2017-05-26 21:27:12 -0400

    Now allows for entering, various event frameworks

--- actions.js
+++ actions.js
@@ -1,42 +1,76 @@
+var loc = "chrisperkins:~$ "
+
 window.onload = function ()
 {
-        document.getElementById("terminal").innerHTML += "<p>C:/chrisperkins/me " +//non-editable location
+    document.getElementById("terminal").innerHTML += "<p>"+ loc +//non-editable location
             "<span id=\"input\" contenteditable=\"true\"></span></p>";//editable text
-        document.getElementById("terminal").addEventListener("paste", handlePaste);
-        document.getElementById("terminal").addEventListener("click", focusInput());
-
-        focusInput();
+    
+    // Prevent paste
+    document.getElementById("terminal").addEventListener("paste", handlePaste);
+    // Focus on click
+    document.getElementById("terminal").addEventListener("click", clickFunction);
+    // New line on enter
+    document.getElementById("terminal").addEventListener("keydown", enterCheck);
+    focusInput();
 }
 
-// Disallow html paste
+function clickFunction(e)
+{
+    //placeCaretAtEnd(document.getElementById("input"));
+    focusInput();
+}
+
+// Disallow paste
 function handlePaste (e)
 {
     // Stop data being directly pasted into div
     e.stopPropagation();
     e.preventDefault();
-
-    /* Get pasted data via clipboard API
-    clipboardData = e.clipboardData || window.clipboardData;
-    pastedData = clipboardData.getData('Text');
-
-    insertAtCaret(pastedData)*/
 }
 
+// Focus on input line
 function focusInput()
 {
     document.getElementById("input").focus();
 }
 
-function brIfEnter(e)
+function enterCheck(e)
 {
-    alert("...");
+    // If we pressed enter...
     if (e.keyCode == 13)
     {
-        alert("???");
-        document.getElementById("input").className = "pastInput";
-        document.getElementById("terminal").innerHTML = "<br>";
+        // Do something with the text entered...
+        //alert(document.getElementById("input").textContent);
+
+        // Make this line non-editable
+        document.getElementById("input").contentEditable = false;
+        // No longer a valid input, so set it to past input
+        document.getElementById("input").setAttribute("id", "pastInput");
         
-        document.getElementById("terminal").innerHTML += "<p>C:/chrisperkins/me " +//non-editable location
-                "<span id=\"input\" contenteditable=\"true\"></span></p>";//editable text
+        // Append our line beginning again!
+        document.getElementById("terminal").innerHTML += "<p>" + loc + //non-editable location
+            "<span id=\"input\" contenteditable=\"true\"></span></p>";//editable text
+        focusInput();
     } 
+}
+
+function placeCaretAtEnd(el) {
+    el.focus();
+    if (typeof window.getSelection != "undefined"
+            && typeof document.createRange != "undefined") 
+    {
+        var range = document.createRange();
+        range.selectNodeContents(el);
+        range.collapse(false);
+        var sel = window.getSelection();
+        sel.removeAllRanges();
+        sel.addRange(range);
+    } 
+    else if (typeof document.body.createTextRange != "undefined")
+    {
+        var textRange = document.body.createTextRange();
+        textRange.moveToElementText(el);
+        textRange.collapse(false);
+        textRange.select();
+    }
 }
