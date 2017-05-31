e6d77f7700798a8f3861c0fd196dfb58e1a19698
neptunicjenkins <chrisfromtemporaryid@gmail.com>
2017-05-30 23:14:39 -0400

    Command rework, still need to fix

--- actions.js
+++ actions.js
@@ -1,9 +1,11 @@
-loc = "chrisperkins:~$ ";
-ver = "1.0.9";
+const loc = "chrisperkins:~$ ";
+const ver = "1.1.0";
 //global colors
-yellow = "#f2ef48";
-white = "white";
-red = "#ff5b5b"
+const yellow = "#f2ef48";
+const white = "white";
+const red = "#ff5b5b"
+terminal = null;
+
 // Speed on typewriter call
 timeConstant = 30;
 
@@ -11,26 +13,139 @@ timeConstant = 30;
 var time = 0
 var curID = 0;
 
+class Command 
+{
+    constructor(action, func)
+    {
+        this.action = function()
+            {
+                terminal.innerHTML += "<span id={0} style='color:{1}'></span>".format(curID, white);
+                
+                prevID = curID;
+                curID += 1;
+                
+                action();
+            }
+        this.description = description;
+    }
+    run(args)
+    {
+        this.func(args);
+    }
+}
+const Commands = 
+{
+    /*"help": new Command(function() 
+    {
+        for(command in Commands)
+        {
+            printToElementWithID("{0} - {1}".format(command, Commands[command].description));
+        }
+
+        printInputLine();
+    },
+    "Displays all commands and their descriptions"),
+
+    "about": new Command(function()
+        {
+            printToElementWithID("ABOUT THE AUTHOR:<br>" + 
+                                 "-----------------", curID);
+            typeWriter("Firstly, thank you for your interest! It means a lot. :)<br><br>" + 
+                    "I'm a junior computer science student at the University of Central Florida,<br>" + 
+                    "and I have a passion for creative, efficient, and simple solutions.<br><br>" + 
+                    "In the next year, I plan to accomplish the following:<br><br>" + 
+                    "Complete:<br>" + 
+                    "Learn Python<br>" + 
+                    "Learn about Hybrid and Native mobile Development<br>" + 
+                    "Learn about SalesForce<br>" + 
+                    "Participate in my first hackathon<br>" +
+                    "<br>" + 
+                    "In Progress:<br>" + 
+                    "Complete 800 CodeForces Problems (300/800)<br>" + 
+                    "Complete the Coursera Machine Learning Course<br>" +
+                    "<br>" + 
+                    "To-Do:<br>" + 
+                    "Complete the Coursera Algorithms I Course<br>" + 
+                    "Complete the Stanford Databases Course<br>" + 
+                    "Join the UCF Programming Team<br>" + 
+                    "<br>" + 
+                    "All of my work so far has been based in Orlando, Florida.<br>" + 
+                    "Despite this fact, I'm open to all options that are based in the United States.<br>" + 
+                    "I'm particularly looking for opportunities that would allow me to learn from and " + 
+                    "contribute to a team's operations in a meaningful way.<br><br>"
+                    , "color:{0}".format(white), printInputLine);
+        },
+        "Tells you a little bit about me"),
+
+    "contact": new Command(function()
+        {
+            printToElementWithID("email - christopherpaulperkins@gmail.com<br>" + 
+                        "phone number - (352)459-9716<br>", prevID);
+        
+            typeWriter("Note: I may not respond to phone calls as I may be in class or at work.<br><br>", 
+                        "color:{0};font-style:italic".format(yellow), printInputLine);
+        }, "Displays my contact information"),
+
+    "github": new Command(function()
+        {
+            printToElementWithID("<a href='https://github.com/Chris-Perkins' target='_blank'>" + 
+                        "My GitHub Profile</a><br><br>", prevID);
+
+            printInputLine();
+        }, 
+        "Displays a clickable link to my GitHub account"),
+        
+    "resume": new Command(function()
+        {
+            printToElementWithID("<a href='' target='_blank'>" + 
+                        "My Resume</a><br><br>", prevID);
+            printInputLine();
+        }, 
+        "Displays a clickable link to my resume (not yet active)")*/
+}
+
+function getCommand(commandID)
+{
+    // Do nothing on empty input
+    if (commandID === "")
+    {
+        printInputLine();
+    }
+    else if (typeof Commands[commandID] === "undefined")
+    {
+        terminal.innerHTML += "<span id={0} style='color:{1}'></span>".format(curID, white);
+        
+        prevID = curID;
+        curID += 1;
+        printToElementWithID("'{0}' is not a valid command.<br>".format(commandID) + 
+                             " Enter 'help' to view a list of available commands.<br><br>", prevID);
+        printInputLine()
+    }
+    else
+    {
+        Commands[commandID].action();
+    }
+}
+
 // When the document opens
 window.onload = function ()
 {
+    terminal = document.getElementById("terminal");
     loadFunctions();
-
     // Prevent paste
-    document.getElementById("terminal").addEventListener("paste", handlePaste);
+    terminal.addEventListener("paste", handlePaste);
     // Newline on enter press, focus input on keypress
     document.addEventListener("keydown", keyCheck);
-    
     launchSequence();
 }
 
 // Sequence that occurs when user 
 function launchSequence()
 {
-    printToElement("<span style='color:{0}'>".format(white) + 
+    printToElementWithID("<span style='color:{0}'>".format(white) + 
                     "ChrisPerkins.me - Home of your next Recruit [Version {0}]<br><br>".format(ver) +
                     "</span>", "terminal");
-    
+
     typeWriter("ADMIN: CHRISTOPHER PERKINS<br>" + 
                 "STATUS: OPEN TO INTERNSHIP OPPORTUNITIES FOR SUMMER 2018<br><br>" +
                 "MISSION STATEMENT:<br>" + 
@@ -41,7 +156,7 @@ function launchSequence()
 }
 
 // Add "htmlString" to element with id "id"
-function printToElement(htmlString, id)
+function printToElementWithID(htmlString, id)
 {
     document.getElementById(id).innerHTML += htmlString;
 }
@@ -52,7 +167,7 @@ function typeWriter(text, style, endFunction)
     // Set time to timeContant (last dialogue may have been skipped)
     time = timeConstant;
     // Create a new element
-    printToElement("<span id='{0}' style='{1}'></span>".format(curID, style), "terminal");
+    printToElementWithID("<span id='{0}' style='{1}'></span>".format(curID, style), "terminal");
     
     typeWriterHelper(text, 0, "", curID, function()
                                         {
@@ -80,7 +195,7 @@ function typeWriterHelper(text, n, currentHTMLString, id, endFunction)
     if (n === text.length || time === 0)
     {
         // Print remaining string if skipped
-        printToElement(currentHTMLString + text.substr(n), id);
+        printToElementWithID(currentHTMLString + text.substr(n), id);
 
         endFunction();
 
@@ -98,7 +213,7 @@ function typeWriterHelper(text, n, currentHTMLString, id, endFunction)
             }
             else
             {
-                printToElement(text[n], id);
+                printToElementWithID(text[n], id);
             }
         }
         else
@@ -108,7 +223,7 @@ function typeWriterHelper(text, n, currentHTMLString, id, endFunction)
             // If close an html string
             if (text[n] == ">")
             {
-                printToElement(currentHTMLString, id);
+                printToElementWithID(currentHTMLString, id);
                 currentHTMLString = ""
                 extraTimeOut = 200;
             }
@@ -124,8 +239,8 @@ function typeWriterHelper(text, n, currentHTMLString, id, endFunction)
 function printInputLine()
 {
     // Terminal line
-    document.getElementById("terminal").innerHTML += "<span style='color:#50e077'>{0}</span>".format(loc)
-    printToElement("<span id='input' contenteditable='true'></span>", "terminal");//editable text
+    terminal.innerHTML += "<span style='color:#50e077'>{0}</span>".format(loc)
+    printToElementWithID("<span id='input' contenteditable='true'></span>", "terminal");//editable text
 
     document.getElementById("input").scrollIntoView(true);
     focusInput();
@@ -149,11 +264,13 @@ function focusInput()
 // Otherwise, skips past dialogue animation
 function keyCheck(e)
 {
+    inputElement = document.getElementById("input");
+
     // If a valid entry point exists...
-    if (document.getElementById("input"))
+    if (inputElement)
     {
         // If we didn't type in the input box, force caret to end of line
-        if (e.srcElement != document.getElementById("input"))
+        if (e.srcElement != inputElement)
         {
             placeCaretAtInputEnd();
         }
@@ -161,15 +278,14 @@ function keyCheck(e)
         if (e.keyCode == 13)
         {
             // Line break since enter was pressed
-            printToElement("<br>", "terminal");
-
-            // Get the command for the text entered
-            getCommand(document.getElementById("input").textContent);
+            printToElementWithID("<br>", "terminal");
 
             // Make this line non-editable
             document.getElementById("input").contentEditable = false;
             // No longer a valid input, so set it to past input
             document.getElementById("input").setAttribute("id", "pastInput");
+
+            getCommand(inputElement.textContent);
         }
     }
     // If a valid entrypoint does not exist, skip dialogue.
@@ -200,93 +316,6 @@ function placeCaretAtInputEnd() {
         textRange.moveToElementText(el);
         textRange.collapse(false);
         textRange.select();
-    }
-}
-
-// Parse user input for command
-function getCommand(text)
-{
-    // span for new print
-    document.getElementById("terminal").innerHTML += 
-            "<span id={0} style='color:{1}'></span>".format(curID, white);
-    prevID = curID;
-    
-    curID += 1;
-    
-    if (text.toLowerCase().startsWith("about"))
-    {
-        typeWriter("ABOUT THE AUTHOR:<br>" + 
-                   "-----------------<br>" + 
-                    "Firstly, thank you for your interest! It means a lot. :)<br><br>" + 
-                    "I'm a junior computer science student at the University of Central Florida,<br>" + 
-                    "and I have a passion for creative, efficient, and simple solutions.<br><br>" + 
-                    "In the next year, I plan to accomplish the following:<br><br>" + 
-                    "Complete:<br>" + 
-                    "Learn Python<br>" + 
-                    "Learn about Hybrid and Native mobile Development<br>" + 
-                    "Learn about SalesForce<br>" + 
-                    "Participate in my first hackathon<br>" +
-                    "<br>" + 
-                    "In Progress:<br>" + 
-                    "Complete 800 CodeForces Problems (300/800)<br>" + 
-                    "Complete the Coursera Machine Learning Course<br>" +
-                    "<br>" + 
-                    "To-Do:<br>" + 
-                    "Complete the Coursera Algorithms I Course<br>" + 
-                    "Complete the Stanford Databases Course<br>" + 
-                    "Join the UCF Programming Team<br>" + 
-                    "<br>" + 
-                    "All of my work so far has been based in Orlando, Florida.<br>" + 
-                    "Despite this fact, I'm open to all options that are based in the United States.<br>" + 
-                    "I'm particularly looking for opportunities that would allow me to learn from and " + 
-                    "contribute to a team's operations in a meaningful way.<br><br>"
-                    , "color:{0}".format(white), printInputLine);
-    }
-    else if (text.toLowerCase().startsWith("contact"))
-    {
-        printToElement("email - christopherpaulperkins@gmail.com<br>" + 
-                        "phone number - (352)459-9716<br>", prevID);
-        
-        typeWriter("Note: I may not respond to phone calls as I may be in class or at work.<br><br>", 
-                   "color:{0};font-style:italic".format(yellow), printInputLine);
-
-    }
-    else if (text.toLowerCase().startsWith("help"))
-    {
-        printToElement("You have access to the following commands:<br>" +
-                       "&nbsphelp - view available commands<br>" + 
-                       "&nbspabout - tells you about me<br>" + 
-                       "&nbspgithub - view my GitHub Link<br>" + 
-                       "&nbspcontact - display my contact information<br>" +
-                       "<br>", prevID);
-        
-        printInputLine();
-    }
-    else if (text.toLowerCase().startsWith("github"))
-    {
-        printToElement("<a href='https://github.com/Chris-Perkins' target='_blank'>" + 
-                        "My GitHub Profile</a><br><br>", prevID);
-
-        printInputLine();
-    }
-    else if (text.toLowerCase().startsWith("resume"))
-    {
-        printToElement("<a href='' target='_blank'>" + 
-                        "My Resume</a><br><br>", prevID);
-        printInputLine();
-    }
-    // Do nothing.
-    else if (text === "")
-    {
-        printInputLine();
-    }
-    // Otherwise, invalid command
-    else
-    {
-        printToElement("'{0}' is an invalid command.<br>".format(text) +
-                        "Enter 'help' to view a list of available commands.<br><br>", prevID);
-        
-        printInputLine();
     }
 }
 
