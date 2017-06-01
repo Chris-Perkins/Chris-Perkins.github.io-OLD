7ed07806231199e497f88d04d16913e8adb31450
perkins.christopher@siemens.com <perkins.christopher@siemens.com>
2017-06-01 16:26:17 -0400

    Complete makeover of screen appearance, typeWriter function.

--- actions.js
+++ actions.js
@@ -1,18 +1,25 @@
 const loc = "chrisperkins:~$ ";
-const ver = "1.1.2";
+const ver = "2.0.0";
 //global colors
 const green = "#50e077";
 const yellow = "#ede671";
 const white = "white";
 const red = "#ff5b5b"
-terminal = null;
+// Typewriter speed if not defined
+const timeConstant = 30;
 
-// Speed on typewriter call
-timeConstant = 30;
+// action keys (ctrl, shift, command, num lock... etc)
+const actionKeycodes = [27, 16, 17, 18, 20, 144,
+                        37, 38, 39, 40, 112, 113, 114,
+                        115, 116, 117, 118, 119, 120,
+                        121, 122, 123, 224, 91]
+
+terminal = null;
 
 // Current speed for typewriter
 var time = 0
 var curID = 0;
+var overrideTypeWriter = false;
 
 // All commands have a function and description
 class Command 
@@ -21,7 +28,7 @@ class Command
     {
         this.action = function()
             {
-                terminal.innerHTML += "<span id={0} style='color:{1}'></span>".format(curID, white);
+                printToElementWithID("<span id={0} style='color:{1}'></span>".format(curID, white), "terminal");
                 
                 action();
             }
@@ -51,15 +58,18 @@ const Commands =
                                  "ADMIN'S AUTOBIOGRAPHY:<br>" + 
                                  "----------------------<br>" + 
                                  "</span>", curID);
-            typeWriter("Firstly, thank you for your interest! It means a lot. :)<br><br>" + 
-                        "I am a junior computer science student at the University of Central Florida,<br>" + 
-                        "and I have a passion for creative, efficient, and simple solutions.<br><br>" + 
-                        "You can enter the command 'goals' to view my Computer Science goals for the year.<br><br>" + 
-                        "All of my work so far has been based in Orlando, Florida.<br>" + 
-                        "Despite this fact, I'm open to all options that are based in the United States.<br><br>" + 
-                        "I am particularly interested in opportunities that would allow me to learn from and " + 
-                        "contribute to a team's operations in a meaningful way.<br><br>"  
-                        , "color:{0}".format(white), function(){printInputLine();curID += 1;});
+            typeWriter([{"text": 
+                            "Firstly, thank you for your interest! It means a lot. :)<br><br>" + 
+                            "I am a junior computer science student at the University of Central Florida,<br>" + 
+                            "and I have a passion for creative, efficient, and simple solutions.<br><br>" + 
+                            "You can enter the command 'goals' to view my Computer Science goals for the year.<br><br>" + 
+                            "All of my work so far has been based in Orlando, Florida.<br>" + 
+                            "Despite this fact, I'm open to all options that are based in the United States.<br><br>" + 
+                            "I am particularly interested in opportunities that would allow me to learn from and " + 
+                            "contribute to a team's operations in a meaningful way.<br><br>",  
+                        "style": 
+                            "color:{0}".format(white)}],
+                        function(){curID+=1;printInputLine();});
         }, "Tells you a little bit about me"),
 
     "contact": new Command(function()
@@ -69,10 +79,9 @@ const Commands =
                                  "<span style='color:{0}'>".format(yellow) + 
                                  "Note: I may not respond to phone calls as I may be in class or at work." + 
                                  "<br><br></span>", curID);
-
-                printInputLine();
                 
                 curID += 1;
+                printInputLine();
         }, "Displays my contact information"),
 
     "clean": new Command(function()
@@ -80,8 +89,8 @@ const Commands =
             printToElementWithID("Cookies have been cleaned!<br><br>", curID);
 
             localStorage.clear();
-            printInputLine();
             curID += 1;
+            printInputLine();
         }, "cleans saved cookies"),
 
     "github": new Command(function()
@@ -89,33 +98,36 @@ const Commands =
             printToElementWithID("<a href='https://github.com/Chris-Perkins' target='_blank'>" + 
                         "My GitHub Profile</a><br><br>", curID);
 
-            printInputLine();
             curID += 1;
+            printInputLine();
         }, 
         "Displays a clickable link to my GitHub account"),
     
     "goals": new Command(function()
         {
+            
             printToElementWithID("<span style='color:{0}'><br>".format(yellow) + 
                                  "ADMIN MISSION LOG<br>" + 
                                  "-----------------<br>" + 
                                  "</span>", curID);
             curID += 1;
-            typeWriter("Complete:<br>" + 
-                        "Learn Python<br>" + 
-                        "Learn about Hybrid and Native mobile Development<br>" + 
-                        "Learn about SalesForce<br>" + 
-                        "Participate in my first hackathon<br>" +
-                        "<br>" + 
-                        "In Progress:<br>" + 
-                        "Complete 800 CodeForces Problems (300/800)<br>" + 
-                        "Complete the Coursera Machine Learning Course<br>" +
-                        "<br>" + 
-                        "To-Do:<br>" + 
-                        "Complete the Coursera Algorithms I Course<br>" + 
-                        "Complete the Stanford Databases Course<br>" + 
-                        "Join the UCF Programming Team<br><br>", 
-                        "color:{0}".format(white), function(){printInputLine();curID += 1;})
+            typeWriter([{"text": 
+                            "Complete:<br>" + 
+                            "Learn Python<br>" + 
+                            "Learn about Hybrid and Native mobile Development<br>" + 
+                            "Learn about SalesForce<br>" + 
+                            "Participate in my first hackathon<br>" +
+                            "<br>" + 
+                            "In Progress:<br>" + 
+                            "Complete 800 CodeForces Problems (300/800)<br>" + 
+                            "Complete the Coursera Machine Learning Course<br>" +
+                            "<br>" + 
+                            "To-Do:<br>" + 
+                            "Complete the Coursera Algorithms I Course<br>" + 
+                            "Complete the Stanford Databases Course<br>" + 
+                            "Join the UCF Programming Team<br><br>", 
+                        "style": "color:{0}".format(white)}], 
+                        function(){printInputLine();curID+=1;})
         }, "My goals for the year"),
         
     "resume": new Command(function()
@@ -176,42 +188,81 @@ function launchSequence()
 
     if(!localStorage.visitCount)
     {
-        typeWriter("ADMIN: CHRISTOPHER PERKINS<br>" + 
-                   "STATUS: OPEN TO INTERNSHIP OPPORTUNITIES FOR SUMMER 2018<br><br>" +
-                   "MISSION STATEMENT:<br>" + 
-                   "Beauty lies in an overarching simplicity;<br>" + 
-                   "The best code makes complex ideas simple.<br>" + 
-                   "Code should say and do more with less.<br><br>" + 
-                   "Enter 'help' to get started.<br><br>", "color:{0}".format(yellow), 
-                   function(){printInputLine();curID+=1;});
+        typeWriter([{
+                     "text": 
+                        "ADMIN: CHRISTOPHER PERKINS<br>" + 
+                        "STATUS: OPEN TO INTERNSHIP OPPORTUNITIES FOR SUMMER 2018<br><br>" +
+                        "MISSION STATEMENT:<br>" + 
+                        "Beauty lies in an overarching simplicity;<br>" + 
+                        "The best code makes complex ideas simple.<br>" + 
+                        "Code should say and do more with less.<br><br>" + 
+                        "Enter 'help' to get started.<br><br>", 
+                     "style": 
+                        "color:{0}".format(yellow)
+                    }], 
+                   function(){curID+=1;printInputLine();});
 
         localStorage.visitCount = 1;
     }
     else
     {
-        typeWriter("ADMIN: CHRISTOPHER PERKINS<br>" + 
-            "STATUS: OPEN TO INTERNSHIP OPPORTUNITIES FOR SUMMER 2018<br><br>" +
-            "MISSION STATEMENT:<br>" + 
-            "Beauty lies in an ove-<br>", "color:{0}".format(yellow), 
-            function()
-            {
-                curID += 1;
-
-                typeWriter("  ...", "color:{0}".format(red), 
-                function()
-                {
-                    curID += 1;
-
-                    typeWriter("<br> Oh. You've been here before.<br>", "color:{0}".format(red), function()
-                        {
-                            curID += 1;
-
-                            typeWriter("Why not get in touch with me? Type 'contact' to view my contact details.<br><br>" + 
-                                       "Otherwise, type 'help' to view all available commands.<br><br>",
-                                       "color:{0}".format(red), function(){printInputLine();curID+=1;});
-                        }, 100) 
-                }, 1000)
-            }, 15);
+        typeWriter(
+                    [{
+                        "text": 
+                            "ADMIN: CHRISTOPHER PERKINS<br>" + 
+                            "STATUS: OPEN TO INTERNSHIP OPPORTUNITIES FOR SUMMER 2018<br><br>" +
+                            "MISSION STATEMENT:<br>" + 
+                            "Beauty lies in an ove-<br><br>", 
+                        "style": 
+                            "color:{0}".format(yellow),
+                        "time": 10
+                    },
+                    {
+                        "text": 
+                            " ...",
+                            "style": 
+                            "color:{0}".format(red),
+                        "time": 1000
+                    },
+                    {
+                        "text": 
+                            "<br> Oh. You've been here before.",
+                        "style": 
+                            "color:{0}".format(red),
+                        "time": 100
+                    },
+                    {
+                        "text": 
+                            "<br><br>Why not get in touch with me?<br>" + 
+                            "Type '",
+                        "style":
+                            "color:{0}".format(white)
+                    },
+                    {
+                        "text": 
+                            "contact",
+                        "style":
+                            "color:{0}".format(yellow)
+                    },
+                    {
+                        "text": "' to view my contact details.<br>" + 
+                            "Otherwise, type '",
+                      "style":
+                            "color:{0}".format(white)
+                    },
+                    {
+                        "text": 
+                            "help",
+                        "style":
+                            "color:{0}".format(yellow)
+                    },
+                    {
+                        "text":
+                            "' to view all available commands.<br><br>",
+                        "style":
+                            "color:{0}".format(white)
+                    }], 
+                    function(){curID+=1;printInputLine();});
 
         localStorage.visitCount += 1;
     }
@@ -224,69 +275,77 @@ function printToElementWithID(htmlString, id)
 }
 
 // Custom type writer function
-function typeWriter(text, style, endFunction, inTime)
-{
-    // Set time to timeContant (last dialogue may have been skipped)
-    time = inTime || timeConstant;
-    // Create a new element
-    printToElementWithID("<span id='{0}' style='{1}'></span>".format(curID, style), "terminal");
-    
-    typeWriterHelper(text, 0, "", curID, endFunction);
+function typeWriter(array, endFunction)
+{   
+    typeWriterHelper(array, 0, 0, "", curID, endFunction);
 }
 
 // Type writer function helper
 // Prints text recursively at a set timer
 // Parses out single tag html and adds to innerhtml separately.
 // "<", ">"" define an html bracket
-function typeWriterHelper(text, n, currentSavedString, id, endFunction)
+function typeWriterHelper(printArray, arrayIndex, strIndex, 
+                          currentSavedString, id, endFunction)
 {
-    // Possibility of <br> string, scroll
-    document.getElementById(curID).scrollIntoView(true);
+    // Base case: went through all array indices
+    if (arrayIndex == printArray.length)
+    {
+        endFunction();
+        return;
+    }
+
+    // On first strIndex, create our span and set time
+    if (strIndex == 0)
+    {
+        printToElementWithID("<span id={0} style='{1}'></span>".format(id, printArray[arrayIndex]["style"]), "terminal")
+        curID += 1;
+        
+        time = printArray[arrayIndex]["time"]||timeConstant;
+    }
+
+    document.getElementById(id).scrollIntoView(true);
 
     // extra time-out used on html tag end
     extraTimeOut = 0;
 
-    // Base case: reached end of string
-    // Alternate case: user skipped dialogue
-    if (n === text.length || time === 0)
-    {
-        // Print remaining string if skipped
-        printToElementWithID(currentSavedString + text.substr(n), id);
-        
-        endFunction();
 
-        return;
+    // Base case 2: reached end of string
+    // Alternate case: user skipped dialogue
+    if (strIndex === printArray[arrayIndex]["text"].length || overrideTypeWriter)
+    {
+        printToElementWithID(currentSavedString + printArray[arrayIndex]["text"].substr(strIndex), id);
+
+        typeWriterHelper(printArray, arrayIndex + 1, 0, "", curID, endFunction);
     }
     else
     {
         // If not an html tag
         if (currentSavedString == "")
         {
-            // If start of tag
-            if (text[n] == "<")
+            if (printArray[arrayIndex]["text"][strIndex] == "<")
             {
-                currentSavedString += text[n];
+                currentSavedString += printArray[arrayIndex]["text"][strIndex];
             }
             else
             {
-                printToElementWithID(text[n], id);
+                printToElementWithID(printArray[arrayIndex]["text"][strIndex], id);
             }
         }
         else
         {
-            currentSavedString += text[n];
+            currentSavedString += printArray[arrayIndex]["text"][strIndex];
             
             // If close an html string
-            if (text[n] == ">")
+            if (printArray[arrayIndex]["text"][strIndex] == ">")
             {
                 printToElementWithID(currentSavedString, id);
-                currentSavedString = ""
-                extraTimeOut = 400;
+                currentSavedString = "";
+                extraTimeOut = time * 15;
             }
         }
         setTimeout(function()
         {
-            typeWriterHelper(text, n + 1, currentSavedString, id, endFunction)
+            typeWriterHelper(printArray, arrayIndex, strIndex + 1, currentSavedString, id, endFunction)
         }, time + extraTimeOut);
     }
 }
@@ -294,6 +353,9 @@ function typeWriterHelper(text, n, currentSavedString, id, endFunction)
 // Print the line for user input
 function printInputLine()
 {
+    // We know typeWriter has ended when this is called.
+    overrideTypeWriter = false;
+
     // Terminal line
     terminal.innerHTML += "<span style='color:{0}'>{1}</span>".format(green, loc)
     printToElementWithID("<span id='input' contenteditable='true'></span>", "terminal");//editable text
@@ -314,34 +376,36 @@ function handlePaste (e)
 // Otherwise, skips past dialogue animation
 function keyCheck(e)
 {
-    inputElement = document.getElementById("input");
-
-    // If a valid entry point exists...
-    if (inputElement)
+    if (actionKeycodes.indexOf(e.keyCode) == -1)
     {
-        // If we didn't type in the input box, force caret to end of line
-        if (e.srcElement != inputElement)
+        inputElement = document.getElementById("input");
+        // If a valid entry point exists...
+        if (inputElement)
         {
-            placeCaretAtInputEnd();
+            // If we didn't type in the input box, force caret to end of line
+            if (e.srcElement != inputElement)
+            {
+                placeCaretAtInputEnd();
+            }
+            // If we pressed enter...
+            if (e.keyCode == 13)
+            {
+                // Line break since enter was pressed
+                printToElementWithID("<br>", "terminal");
+
+                // Make this line non-editable
+                document.getElementById("input").contentEditable = false;
+                // No longer a valid input, so set it to past input
+                document.getElementById("input").setAttribute("id", "pastInput");
+
+                getCommand(inputElement.textContent);
+            }
         }
-        // If we pressed enter...
-        if (e.keyCode == 13)
+        // If a valid entrypoint does not exist, skip dialogue.
+        else
         {
-            // Line break since enter was pressed
-            printToElementWithID("<br>", "terminal");
-
-            // Make this line non-editable
-            document.getElementById("input").contentEditable = false;
-            // No longer a valid input, so set it to past input
-            document.getElementById("input").setAttribute("id", "pastInput");
-
-            getCommand(inputElement.textContent);
+            overrideTypeWriter = true;
         }
-    }
-    // If a valid entrypoint does not exist, skip dialogue.
-    else
-    {
-        time = 0;
     }
 }
 
